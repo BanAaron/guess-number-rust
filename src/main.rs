f6fd2157f9f223b9004a78cdc438121da9d23659
Aaron Barratt <aarontbarratt@gmail.com>
2023-05-02 16:28:59 +0100

    1️⃣ first commmit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, World!");
+    println!("Guess the number!");
+    // mut means mutable
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line!");
+
+    print!("hello");
 }
